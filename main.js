10327542e173ee78fcab643e7ec6975fc5bf5b98
Genola Johnson <genolajohnson@gmail.com>
2020-06-16 22:44:30 +0000

    database ready for objects

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 import customers from './customers.js'
+console.log(customes.js);
+
+let dataBase = {
+    name:
+    street:
+    phoneNumber:
+    thumbNail:
+    dateOfBirth:
+    dateBecameCustomer:
+}
+
+
+{
+   function dataBase(dataArray)
+    let custName = document.querySelector(('#custName')
+
+        
+    }
+}
+listData(dataBase)
